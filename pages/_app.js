5dce3d6ae0333951a2e5c89933ee786ed8b606c5
do-sieg <saleth.admin@gmail.com>
2022-12-15 23:41:47 +0100

    Added Google Analytics component

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "nextjs-google-analytics";
 import SettingsProvider from "../components/context/SettingsProvider";
 import Layout from "../components/ui/Layout";
 import "../styles/globals.css";
@@ -6,6 +7,10 @@ export default function MyApp({ Component, pageProps }) {
 	return (
 		<SettingsProvider>
 			<Layout>
+				<GoogleAnalytics
+					gaMeasurementId="G-446BYMFFEK"
+					trackPageViews={{ ignoreHashChange: true }}
+				/>
 				<Component {...pageProps} />
 			</Layout>
 		</SettingsProvider>
