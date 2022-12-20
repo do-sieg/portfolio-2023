c7afd1e591b1a61e3e2c5909d423bddaf9974c3d
do-sieg <saleth.admin@gmail.com>
2022-12-20 10:09:35 +0100

    Added support for credits component multiple authors

--- components/ui/Credits.jsx
+++ components/ui/Credits.jsx
@@ -6,7 +6,23 @@ const styles = {
 }
 
 export default function Credits({ text, name, link }) {
-    return <span style={styles.container}>
-        {text} {link ? <a href={link} target="_blank" rel="noreferrer">{name}</a> : <span>{name}</span>}
-    </span>;
+
+    function renderNames() {
+        const names = Array.isArray(name) ? name : [name];
+        const links = Array.isArray(link) ? link : [link];
+
+        return names.map((name, index) => {
+            return (
+                <>
+                    {link[index] ?
+                        <a href={links[index]} target="_blank" rel="noreferrer">{name}</a>
+                        :
+                        <span>{name}</span>}
+                    {index < names.length - 1 && <span>, </span>}
+                </>
+            );
+        });
+    }
+
+    return <span style={styles.container}> {text} {renderNames()}</span>;
 }
