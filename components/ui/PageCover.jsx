ad50ba7236973613f293357a7503ea42b1bb33f0
do-sieg <saleth.admin@gmail.com>
2023-03-07 23:52:05 +0100

    Set cover images to high priority

--- components/ui/PageCover.jsx
+++ components/ui/PageCover.jsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 import styles from "./PageCover.module.css";
 
 export default function PageCover({ src, alt }) {
-    return <Image className={styles.container} src={src} alt={alt} placeholder="blur" />
+    return <Image className={styles.container} src={src} alt={alt} placeholder="blur" priority />
 }
